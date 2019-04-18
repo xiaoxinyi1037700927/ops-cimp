64ff6564dede39d4696268594b07918466e0747c
yuefj <123456>
2019-04-18 14:03:55 +0800

    修正目录错误

--- src/main/java/com/sinosoft/ops/cimp/mapper/user/OrganizationViewMapper.java
+++ src/main/java/com/sinosoft/ops/cimp/mapper/user/OrganizationViewMapper.java
@@ -1,11 +1,8 @@
 package com.sinosoft.ops.cimp.mapper.user;
 
 import com.sinosoft.ops.cimp.entity.oraganization.Organization;
-import com.sinosoft.ops.cimp.vo.to.organization.BusinessUnitOrgListViewModel;
 import com.sinosoft.ops.cimp.vo.to.organization.OrganizationViewModel;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -14,11 +11,5 @@ public interface OrganizationViewMapper {
     OrganizationViewMapper INSTANCE = Mappers.getMapper(OrganizationViewMapper.class);
 
     OrganizationViewModel organizationToViewModel(Organization organization);
-
-    @Mappings({
-            @Mapping(source = "id", target = "organizationId"),
-            @Mapping(source = "name", target = "organizationName"),
-    })
-    BusinessUnitOrgListViewModel organizationToBUOViewModel(Organization organization);
 
 }
