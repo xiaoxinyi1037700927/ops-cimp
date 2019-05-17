926a5da0106856cfc7d4a4da88db44adfbbcacce
huangshaofan <2595476383@qq.com>
2019-05-17 18:05:22 +0800

    忽略MongoDb自动配置

--- src/main/java/com/sinosoft/ops/cimp/CimpApplication.java
+++ src/main/java/com/sinosoft/ops/cimp/CimpApplication.java
@@ -2,10 +2,12 @@ package com.sinosoft.ops.cimp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableSwagger2
 @EnableScheduling
 public class CimpApplication {
