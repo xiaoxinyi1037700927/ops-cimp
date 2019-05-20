43fb5599a6dec7d1bad2bbfb152f7eb648e003cf
rainchen <214042114@qq.com>
2019-05-20 10:14:14 +0800

    Mongodb配置

--- src/main/java/com/sinosoft/ops/cimp/config/MongoDBConfig.java
+++ src/main/java/com/sinosoft/ops/cimp/config/MongoDBConfig.java
@@ -2,47 +2,114 @@ package com.sinosoft.ops.cimp.config;
 
 
 import com.mongodb.MongoClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.ServerAddress;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.data.mongodb.MongoDbFactory;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.data.annotation.Persistent;
+import org.springframework.data.convert.CustomConversions;
+import org.springframework.data.mapping.model.CamelCaseAbbreviatingFieldNamingStrategy;
+import org.springframework.data.mapping.model.FieldNamingStrategy;
+import org.springframework.data.mapping.model.PropertyNameFieldNamingStrategy;
+import org.springframework.data.mongodb.config.MongoConfigurationSupport;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
-@EnableMongoRepositories
-// 读取配置文件的，通过Environment读取
-@PropertySource("classpath:application.properties")
-public class MongoDBConfig  extends  AbstractMongoConfiguration{
+public class MongoDBConfig {
 
-
-    @Autowired
-    Environment env;
-
-    @Bean
-    public GridFsTemplate gridFsTemplate() throws Exception{
-        return new GridFsTemplate(secondaryFactory(),mappingMongoConverter());
-    }
+    @Value("${mongoClientIp}")
+    private String mongoClientIp;
+    @Value("${mongoClientPort}")
+    private Integer mongoClientPort;
+    @Value("${mongoDbName}")
+    private String mongoDbName;
 
     @Bean
-    public MongoDbFactory secondaryFactory() throws Exception {
-        return new SimpleMongoDbFactory(mongoClient(),
-                getDatabaseName());
+    public MongoTemplate mongoTemplate() {
+
+        ServerAddress serverAddress = new ServerAddress(mongoClientIp, mongoClientPort);
+        MongoClientOptions options = MongoClientOptions.builder().requiredReplicaSetName("192.168.0.143:27017").build();
+        MongoClient mongoClient = new MongoClient(serverAddress, options);
+        mongoClient.getMongoClientOptions();
+        SimpleMongoDbFactory simpleMongoDbFactory = new SimpleMongoDbFactory(mongoClient, mongoDbName);
+
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(simpleMongoDbFactory);
+
+        MongoMappingContext mappingContext = new MongoMappingContext();
+
+        try {
+            mappingContext.setInitialEntitySet(this.getInitialEntitySet());
+        } catch (ClassNotFoundException ignored) {
+        }
+        mappingContext.setFieldNamingStrategy(this.fieldNamingStrategy());
+        mappingContext.setSimpleTypeHolder(this.customConversions().getSimpleTypeHolder());
+
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mappingContext);
+        converter.setCustomConversions(new MongoCustomConversions(Collections.emptyList()));
+
+        return new MongoTemplate(simpleMongoDbFactory, converter);
     }
 
-    @Override
-    public MongoClient mongoClient() {
-        return new MongoClient(env.getProperty("spring.data.mongodb.host"),env.getProperty("spring.data.mongodb.port",Integer.class));
+    private Set<Class<?>> getInitialEntitySet() throws ClassNotFoundException {
+        Set<Class<?>> initialEntitySet = new HashSet<>();
+        for (String basePackage : this.getMappingBasePackages()) {
+            initialEntitySet.addAll(this.scanForEntities(basePackage));
+        }
+
+        return initialEntitySet;
     }
 
-    @Override
-    protected String getDatabaseName() {
-        return env.getProperty("spring.data.mongodb.database");
+    private Set<Class<?>> scanForEntities(String basePackage) throws ClassNotFoundException {
+        if (!StringUtils.hasText(basePackage)) {
+            return Collections.emptySet();
+        } else {
+            Set<Class<?>> initialEntitySet = new HashSet<>();
+            if (StringUtils.hasText(basePackage)) {
+                ClassPathScanningCandidateComponentProvider componentProvider = new ClassPathScanningCandidateComponentProvider(false);
+                componentProvider.addIncludeFilter(new AnnotationTypeFilter(Document.class));
+                componentProvider.addIncludeFilter(new AnnotationTypeFilter(Persistent.class));
+
+                for (BeanDefinition candidate : componentProvider.findCandidateComponents(basePackage)) {
+                    initialEntitySet.add(ClassUtils.forName(candidate.getBeanClassName(), MongoConfigurationSupport.class.getClassLoader()));
+                }
+            }
+
+            return initialEntitySet;
+        }
+    }
+
+    private boolean abbreviateFieldNames() {
+        return false;
+    }
+
+    private FieldNamingStrategy fieldNamingStrategy() {
+        return this.abbreviateFieldNames() ? new CamelCaseAbbreviatingFieldNamingStrategy() : PropertyNameFieldNamingStrategy.INSTANCE;
+    }
+
+    private Collection<String> getMappingBasePackages() {
+        Package mappingBasePackage = this.getClass().getPackage();
+        return Collections.singleton(mappingBasePackage == null ? null : mappingBasePackage.getName());
+    }
+
+    public CustomConversions customConversions() {
+        return new MongoCustomConversions(Collections.emptyList());
     }
 }
--- src/main/java/com/sinosoft/ops/cimp/repository/archive/impl/MongoDbDaoImpl.java
+++ src/main/java/com/sinosoft/ops/cimp/repository/archive/impl/MongoDbDaoImpl.java
@@ -16,7 +16,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.io.*;
@@ -28,194 +27,196 @@ import java.util.*;
 @Repository("mongoDbDao")
 public class MongoDbDaoImpl implements MongoDbDao {
 
-  @Autowired
-  private GridFsTemplate gridFsTemplate;
-  @Autowired
-  private MongoTemplate mongo;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
 
-  @Override
-  public String uploadFromFile(File file) throws IOException {
-    return uploadFromFile(file, null);
-  }
-
-  @Override
-  public String uploadFromFile(File file, Map<String, Object> extendDoc) throws IOException {
-    return uploadFileFromStream(file.getName(), new FileInputStream(file), extendDoc);
-  }
-
-  @Override
-  public String uploadFileFromStream(String fileName, InputStream is) throws IOException {
-    return uploadFileFromStream(fileName, is, null);
-  }
-
-  @Override
-  public String uploadFileFromStream(String fileName, InputStream is, Map<String, Object> extendDoc)
-      throws IOException {
-
-    String mongoDbId = genMongoDbId();
-    uploadFileFromStream(mongoDbId, fileName, is, extendDoc);
-
-    return mongoDbId;
-  }
-
-  @Override
-  public void uploadFileFromStream(String mongoDbId, String fileName, InputStream is,
-      Map<String, Object> extDoc) throws IOException {
-    try {
-
-      String contentType = Files.probeContentType(Paths.get(fileName));
-      String fileExtension = com.google.common.io.Files.getFileExtension(fileName);
-
-      GridFSBucket gfsb = GridFSBuckets.create(mongo.getDb());
-
-      Document doc = new Document("extension", fileExtension).append("content_type", contentType);
-      
-      System.out.println("docdoc==="+doc);
-      if (extDoc != null) {
-        doc.putAll(extDoc);
-      }
-      GridFSUploadOptions uploadOptions = new GridFSUploadOptions().metadata(doc);
-
-      gfsb.uploadFromStream(new BsonString(mongoDbId), fileName, is, uploadOptions);
-
-    } finally {
-      IOUtils.closeQuietly(is);
+    @Override
+    public String uploadFromFile(File file) throws IOException {
+        return uploadFromFile(file, null);
     }
 
-  }
+    @Override
+    public String uploadFromFile(File file, Map<String, Object> extendDoc) throws IOException {
+        return uploadFileFromStream(file.getName(), new FileInputStream(file), extendDoc);
+    }
 
-  @Override
-  public GridFSFile downloadFileToStream(String id, OutputStream os)
-      throws IOException, CannotFindMongoDbResourceById {
+    @Override
+    public String uploadFileFromStream(String fileName, InputStream is) throws IOException {
+        return uploadFileFromStream(fileName, is, null);
+    }
 
-    GridFSFile gfsf = null;
-    try {
-      // 获取文件头信息
-      Query queryWhereId = new Query(Criteria.where("_id").is(id));
+    @Override
+    public String uploadFileFromStream(String fileName, InputStream is, Map<String, Object> extendDoc)
+            throws IOException {
 
-      gfsf = gridFsTemplate.findOne(queryWhereId);
-      if (gfsf == null)
-        throw new CannotFindMongoDbResourceById(queryWhereId);
+        String mongoDbId = genMongoDbId();
+        uploadFileFromStream(mongoDbId, fileName, is, extendDoc);
+
+        return mongoDbId;
+    }
+
+    @Override
+    public void uploadFileFromStream(String mongoDbId, String fileName, InputStream is,
+                                     Map<String, Object> extDoc) throws IOException {
+        try {
+
+            String contentType = Files.probeContentType(Paths.get(fileName));
+            String fileExtension = com.google.common.io.Files.getFileExtension(fileName);
+
+            GridFSBucket gfsb = GridFSBuckets.create(mongoTemplate.getDb());
+
+            Document doc = new Document("extension", fileExtension).append("content_type", contentType);
+
+            System.out.println("docdoc===" + doc);
+            if (extDoc != null) {
+                doc.putAll(extDoc);
+            }
+            GridFSUploadOptions uploadOptions = new GridFSUploadOptions().metadata(doc);
+
+            gfsb.uploadFromStream(new BsonString(mongoDbId), fileName, is, uploadOptions);
+
+        } finally {
+            IOUtils.closeQuietly(is);
+        }
+
+    }
+
+    @Override
+    public GridFSFile downloadFileToStream(String id, OutputStream os)
+            throws IOException, CannotFindMongoDbResourceById {
+
+        GridFSFile gfsf = null;
+        try {
+            // 获取文件头信息
+            Query queryWhereId = new Query(Criteria.where("_id").is(id));
+
+            gfsf = mongoTemplate.findOne(queryWhereId, GridFSFile.class);
+//            gfsf = gridFsTemplate.findOne(queryWhereId);
+            if (gfsf == null)
+                throw new CannotFindMongoDbResourceById(queryWhereId);
 //
-      // 获取多块二进制数据,写入到输出流
-      GridFSBucket gfsb = GridFSBuckets.create(mongo.getDb());
-      gfsb.downloadToStream(new BsonString(id), os);
-    } finally {
-      IOUtils.closeQuietly(os);
+            // 获取多块二进制数据,写入到输出流
+            GridFSBucket gfsb = GridFSBuckets.create(mongoTemplate.getDb());
+            gfsb.downloadToStream(new BsonString(id), os);
+        } finally {
+            IOUtils.closeQuietly(os);
+        }
+
+        return gfsf;
     }
 
-    return gfsf;
-  }
+    @Override
+    public File dowloadToFile(String id, Path path)
+            throws IOException, CannotFindMongoDbResourceById {
+        return dowloadToFile(id, path.toString());
+    }
 
-  @Override
-  public File dowloadToFile(String id, Path path)
-      throws IOException, CannotFindMongoDbResourceById {
-    return dowloadToFile(id, path.toString());
-  }
+    @Override
+    public File dowloadToFile(String id, String path)
+            throws IOException, CannotFindMongoDbResourceById {
+        File file = new File(path);
+        dowloadToFile(id, file);
+        return file;
+    }
 
-  @Override
-  public File dowloadToFile(String id, String path)
-      throws IOException, CannotFindMongoDbResourceById {
-    File file = new File(path);
-    dowloadToFile(id, file);
-    return file;
-  }
+    @Override
+    public void dowloadToFile(String id, File file)
+            throws IOException, CannotFindMongoDbResourceById {
+        downloadFileToStream(id, new FileOutputStream(file));
+    }
 
-  @Override
-  public void dowloadToFile(String id, File file)
-      throws IOException, CannotFindMongoDbResourceById {
-    downloadFileToStream(id, new FileOutputStream(file));
-  }
+    @Override
+    public void deleteFileById(String id) {
+        GridFSBucket gfsb = GridFSBuckets.create(mongoTemplate.getDb());
+        gfsb.delete(new BsonString(id));
+    }
 
-  @Override
-  public void deleteFileById(String id) {
-    GridFSBucket gfsb = GridFSBuckets.create(mongo.getDb());
-    gfsb.delete(new BsonString(id));
-  }
+    @Override
+    public GridFSFile findFileById(String id) {
+        Query queryWhereId = new Query(Criteria.where("_id").is(id));
+        return mongoTemplate.findOne(queryWhereId, GridFSFile.class);
+//        GridFSFile gfsf = gridFsTemplate.findOne(queryWhereId);
 
-  @Override
-  public GridFSFile findFileById(String id) {
-    Query queryWhereId = new Query(Criteria.where("_id").is(id));
+//        return gfsf;
+    }
 
-    GridFSFile gfsf = gridFsTemplate.findOne(queryWhereId);
+    @Override
+    public List<GridFSFile> findeFileListById(Collection<String> ids) {
+        List<GridFSFile> list = new ArrayList<GridFSFile>();
+        Query query = new Query(Criteria.where("_id").in(ids));//select * from ** where _id in (ids);
+        GridFSFindIterable iter = mongoTemplate.findOne(query, GridFSFindIterable.class);
 
-    return gfsf;
-  }
-  
-  @Override
-  public List<GridFSFile> findeFileListById(Collection<String> ids){
-	  List<GridFSFile> list = new ArrayList<GridFSFile>();
-	  Query query = new Query(Criteria.where("_id").in(ids));//select * from ** where _id in (ids);
-	  GridFSFindIterable iter =  gridFsTemplate.find(query);
-	  MongoCursor<GridFSFile> mc = iter.iterator();
-	  while(mc.hasNext()){
-		  GridFSFile gsf = mc.next();
-		  list.add(gsf);
-	  }
-	  return list;
-  }
+//        mongoTemplate.findAll(query).var
+//        GridFSFindIterable iter = gridFsTemplate.find(query);
+        MongoCursor<GridFSFile> mc = iter.iterator();
+        while (mc.hasNext()) {
+            GridFSFile gsf = mc.next();
+            list.add(gsf);
+        }
+        return list;
+    }
 
 
-  @Override
-  public List<GridFSFile> findFileByConditions(Bson filters) {
-    return findFileByConditions(filters, null);
-  }
+    @Override
+    public List<GridFSFile> findFileByConditions(Bson filters) {
+        return findFileByConditions(filters, null);
+    }
 
-  @Override
-  public List<GridFSFile> findFileByConditions(Bson filters, Bson sort) {
-    return findFileByConditions(filters, sort, 0);
-  }
+    @Override
+    public List<GridFSFile> findFileByConditions(Bson filters, Bson sort) {
+        return findFileByConditions(filters, sort, 0);
+    }
 
-  @Override
-  public List<GridFSFile> findFileByConditions(Bson filters, Bson sort, int limit) {
-    return findFileByConditions(filters, sort, limit, 0);
-  }
+    @Override
+    public List<GridFSFile> findFileByConditions(Bson filters, Bson sort, int limit) {
+        return findFileByConditions(filters, sort, limit, 0);
+    }
 
-  @Override
-  public List<GridFSFile> findFileByConditions(Bson filters, Bson sort, int limit, int skip) {
-    List<GridFSFile> gfsfs = new ArrayList<GridFSFile>();
-    GridFSBucket gfsb = GridFSBuckets.create(mongo.getDb());
-    gfsb.find(filters).sort(sort).limit(limit).skip(skip).into(gfsfs);
+    @Override
+    public List<GridFSFile> findFileByConditions(Bson filters, Bson sort, int limit, int skip) {
+        List<GridFSFile> gfsfs = new ArrayList<GridFSFile>();
+        GridFSBucket gfsb = GridFSBuckets.create(mongoTemplate.getDb());
+        gfsb.find(filters).sort(sort).limit(limit).skip(skip).into(gfsfs);
 
-    return gfsfs;
+        return gfsfs;
 
-  }
+    }
 
-  @Override
-  public void updateFromFile(String id, File file) throws IOException {
-    updateFromFile(id, file, null);
-  }
+    @Override
+    public void updateFromFile(String id, File file) throws IOException {
+        updateFromFile(id, file, null);
+    }
 
-  @Override
-  public void updateFromFile(String id, File file, Map<String, Object> extendDoc)
-      throws IOException {
-    updateFileFromStream(id, file.getName(), new FileInputStream(file), extendDoc);
-  }
+    @Override
+    public void updateFromFile(String id, File file, Map<String, Object> extendDoc)
+            throws IOException {
+        updateFileFromStream(id, file.getName(), new FileInputStream(file), extendDoc);
+    }
 
-  @Override
-  public void updateFileFromStream(String id, String fileName, InputStream is) throws IOException {
-    updateFileFromStream(id, fileName, is, null);
-  }
+    @Override
+    public void updateFileFromStream(String id, String fileName, InputStream is) throws IOException {
+        updateFileFromStream(id, fileName, is, null);
+    }
 
-  @Override
-  public void updateFileFromStream(String id, String fileName, InputStream is,
-      Map<String, Object> extendDoc) throws IOException {
+    @Override
+    public void updateFileFromStream(String id, String fileName, InputStream is,
+                                     Map<String, Object> extendDoc) throws IOException {
 
-    // 查出以前扩展信息
-    GridFSFile gfsf = findFileById(id);
-    Document doc = gfsf.getMetadata();
-    doc.putAll(extendDoc);
+        // 查出以前扩展信息
+        GridFSFile gfsf = findFileById(id);
+        Document doc = gfsf.getMetadata();
+        doc.putAll(extendDoc);
 
-    // 删除的
-    deleteFileById(id);
+        // 删除的
+        deleteFileById(id);
 
-    uploadFileFromStream(fileName, is, doc);
+        uploadFileFromStream(fileName, is, doc);
 
-  }
+    }
 
-  @Override
-  public String genMongoDbId() {
-    return String.format("%s", UUID.randomUUID());//格式化成字符串
-  }
+    @Override
+    public String genMongoDbId() {
+        return String.format("%s", UUID.randomUUID());//格式化成字符串
+    }
 }
