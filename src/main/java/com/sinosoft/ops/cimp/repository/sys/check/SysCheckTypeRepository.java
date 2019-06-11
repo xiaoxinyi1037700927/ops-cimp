4b90374113f51137ae0c23242a15255d508d85e8
lixiang <99578289@qq.com>
2019-06-11 19:55:17 +0800

    增加

--- src/main/java/com/sinosoft/ops/cimp/repository/sys/check/SysCheckTypeRepository.java
+++ src/main/java/com/sinosoft/ops/cimp/repository/sys/check/SysCheckTypeRepository.java
@@ -1,4 +1,9 @@
 package com.sinosoft.ops.cimp.repository.sys.check;
 
-public interface SysCheckTypeRepository {
+import com.sinosoft.ops.cimp.entity.sys.check.SysCheckCondition;
+import com.sinosoft.ops.cimp.entity.sys.check.SysCheckType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface SysCheckTypeRepository extends JpaRepository<SysCheckType, String>, QuerydslPredicateExecutor<SysCheckType> {
 }
